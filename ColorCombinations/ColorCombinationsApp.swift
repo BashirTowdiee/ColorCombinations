2f8f256f27f04016119ed56b883cc2cd60f41de2
BashirTowdiee <towdiee.bashir@gmail.com>
2026-01-24 21:31:58 +1100

    Initial Commit

---
+++ ColorCombinations/ColorCombinationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorCombinationsApp.swift
+//  ColorCombinations
+//
+//  Created by Bashir Towdiee on 24/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct ColorCombinationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorCombinations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorCombinations
+//
+//  Created by Bashir Towdiee on 24/1/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
